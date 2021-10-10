74c570fb439714bd4a68f5efa7575977006be273
FreyllaR <olkov.kir1@yandex.ru>
2021-10-10 23:55:22 +0300

    number 1 completed

---
+++ src/ru/FreyllaR/exercise.java
@@ -0,0 +1,13 @@
+package ru.FreyllaR;
+
+import java.util.Scanner;
+
+public class exercise {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long a = scan.nextLong();
+        long b = scan.nextLong();
+        System.out.println(a + b);
+    }
+}
